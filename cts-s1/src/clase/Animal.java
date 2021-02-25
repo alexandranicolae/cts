240ecbce0f62d7dbdab0b1355c02e1e969a48bdc
alexandranicolae <alexa.nicolae@yahoo.com>
2021-02-25 17:32:12 +0200

    Adaugat comentariu info abstract

--- cts-s1/src/clase/Animal.java
+++ cts-s1/src/clase/Animal.java
@@ -4,6 +4,10 @@ public abstract class Animal {
 
 	private String name;
 
+	/* 
+	 * clasa abstracta in C are o metoda/more virtual pure, in Java doar punem abstract
+	 * o clasa abstracta nu poate fi instantiata -> o mostenim (derivam) sau facem pointer la acea clasa (doar in C)
+	 * */
 	
 	public Animal(String name) {
 		super();
