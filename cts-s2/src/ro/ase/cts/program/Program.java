48d4c56c5b5d335a511063eb2820ef6b2d773631
alexandranicolae <alexa.nicolae@yahoo.com>
2021-03-04 16:27:21 +0200

    Creare clase readere

--- cts-s2/src/ro/ase/cts/program/Program.java
+++ cts-s2/src/ro/ase/cts/program/Program.java
@@ -3,16 +3,20 @@ package ro.ase.cts.program;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatiReader;
 
 public class Program {
-
+	
+//Principiul Dependency .... "ala de nivel inalt nu trebuie sa depinda de fct de nivel jos"
+//	implementarile pe care le realizati sa nu depinda direct de clase concrete, ci de tipul abstract
+//	nu mai avem List<Angajat> ci List<Aplicant> si e bun si pt student si pt elev si inlocuim dupa doar o linie (AngajatReader)
+	
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = AngajatiReader.readAngajati("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
